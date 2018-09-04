de8fdc5817e076d76723993db1a0f9102c00d85b
huskyhoochu <dfg1499@gmail.com>
2018-09-04 18:02:31 +0900

    [#5] Card 컴포넌트 구현
    - 각 카드의 이름을 Props로 전달
    - Join이 문제인 듯

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,12 +1,27 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 // Styled
 import Styled from '../../styled/base';
 
+const Card = ({ name }) => (
+  <div>
+    <h2>{name}</h2>
+    <p>image</p>
+  </div>
+);
+
 const Main = () => (
   <Styled.FlexWrapper>
-    <h1>Main</h1>
+    <Card name="About" />
+    <Card name="Join" />
+    <Card name="Von Voyage" />
+    <Card name="I'm all ears" />
   </Styled.FlexWrapper>
 );
+
+Card.propTypes = {
+  name: PropTypes.string.isRequired,
+};
 
 export default Main;
