34eac9483dc1b8c0eb1635515330bcd37997b1da
huskyhoochu <dfg1499@gmail.com>
2018-09-05 13:36:19 +0900

    [#5] test 수정
    - Main.test에 MemoryRouter 추가
    
    fix #5

--- src/components/main/Main.test.js
+++ src/components/main/Main.test.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { MemoryRouter } from 'react-router-dom';
 import Main from './Main';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<Main />, div);
+  ReactDOM.render(
+    <MemoryRouter>
+      <Main />
+    </MemoryRouter>,
+    div,
+  );
   ReactDOM.unmountComponentAtNode(div);
 });
