84f82d3c2d78520a4cae2d6eebc279738eb1c46b
huskyhoochu <dfg1499@gmail.com>
2018-09-05 14:51:41 +0900

    [#12] All Ears 컴포넌트 생성
    - 컴포넌트 및 테스트 생성
    - 백엔드 개발되면 Authentication 여부에 따라 바뀌어야 함

---
+++ src/components/all_ears/AllEars.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// Styled
+import StyledBase from '../../styled/Base';
+
+const AllEars = () => (
+  <StyledBase.FlexWrapper>
+    <a href="mailto:webmasterlittera@gmail.com">webmasterlittera@gmail.com</a>
+  </StyledBase.FlexWrapper>
+);
+
+export default AllEars;
