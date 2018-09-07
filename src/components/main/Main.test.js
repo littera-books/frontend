ac34183b6c21d3bd2e104dd7db788ea4bffc0770
huskyhoochu <dfg1499@gmail.com>
2018-09-07 16:05:34 +0900

    [#13] 테스트 케이스 재작성
    - 이벤트 리스너가 담긴 컴포넌트의 테스트를 위해 setup 파일 추가
    - Main 컴포넌트의 state 값 변경 여부 검사

--- src/components/main/Main.test.js
+++ src/components/main/Main.test.js
@@ -1,15 +1,25 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { MemoryRouter } from 'react-router-dom';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import Main from './Main';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(
-    <MemoryRouter>
-      <Main />
-    </MemoryRouter>,
-    div,
-  );
-  ReactDOM.unmountComponentAtNode(div);
+Enzyme.configure({ adapter: new Adapter() });
+
+function setup() {
+  const enzymeWrapper = shallow(<Main />);
+
+  return { enzymeWrapper };
+}
+
+describe('Main', () => {
+  it('state가 기기 width와 동일한가', () => {
+    const { enzymeWrapper } = setup();
+    expect(enzymeWrapper.state('width')).toEqual(window.innerWidth);
+  });
+
+  it('state를 414px 이하로 줄이면 캐러셀 컴포넌트가 실행되는가', () => {
+    const { enzymeWrapper } = setup();
+    enzymeWrapper.setState({ width: 300 });
+    expect(enzymeWrapper.find('#carousel').exists()).toBe(true);
+  });
 });
