40d5079bc2c1350485d0210e56fcc255de67f40a
huskyhoochu <dfg1499@gmail.com>
2018-09-07 15:24:03 +0900

    [#13] 사이트 너비에 따른 타이틀 크기 조절
    - 아이폰 8 해상도(375px)  기준으로 그 이하부터는 타이틀 크기가 작아짐

--- src/components/title/Styled_title.js
+++ src/components/title/Styled_title.js
@@ -4,6 +4,11 @@ const TitleWrapper = styled.div`
   margin: auto;
   padding: 3rem 0;
   max-width: 10rem;
+
+  @media (max-width: 375px) {
+    padding: 3rem 0 1rem 0;
+    max-width: 8rem;
+  }
 `;
 
 export default {
