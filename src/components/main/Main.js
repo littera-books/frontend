bc5f1e478269e2ad45252f61e172a283c8d6cadd
huskyhoochu <dfg1499@gmail.com>
2018-09-07 16:02:27 +0900

    [#13] 너비에 따라 리턴 컴포넌트 변경 로직 작성
    - 데스크탑 너비에서는 원래 형태로 컴포넌트를 보여주고
    - 너비가 아이폰 8 Plus 기준(414px)으로 줄어들면 캐러셀 컴포넌트를
    보여주도록 함

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import Slider from 'react-slick';
 
 // Styled
 import StyledBase from '../../styled/Base';
 import Styled from './Styled_main';
+
+// CSS
+import '../../../node_modules/slick-carousel/slick/slick.css';
+import '../../../node_modules/slick-carousel/slick/slick-theme.css';
 
 // Images
 import About from '../../assets/images/about.jpg';
@@ -19,8 +24,8 @@ const Card = ({ img, name }) => (
   </Styled.CardWrapper>
 );
 
-const Main = () => (
-  <StyledBase.FlexWrapper>
+const CardList = () => (
+  <Fragment>
     <Link to="/about">
       <Card img={About} name="About" />
     </Link>
@@ -29,8 +34,68 @@ const Main = () => (
     <Link to="/all-ears">
       <Card img={AllEars} name="I'm all ears" />
     </Link>
-  </StyledBase.FlexWrapper>
+  </Fragment>
 );
+
+class Main extends React.PureComponent {
+  // 창의 너비가 일정 수준 이하로 좁아지면 화면 구조를 캐러셀로 변화시킨다
+  // EventListener 가 창의 너비를 실시간으로 읽어들인다
+  constructor(props) {
+    super(props);
+    this.state = { width: 0 };
+    this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
+  }
+
+  componentDidMount() {
+    this.updateWindowDimensions();
+    window.addEventListener('resize', this.updateWindowDimensions);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.updateWindowDimensions);
+  }
+
+  updateWindowDimensions() {
+    this.setState({ width: window.innerWidth });
+  }
+
+  render() {
+    const { width } = this.state;
+    const settings = {
+      dots: true,
+      arrows: false,
+      infinite: true,
+      lazyLoad: true,
+      centerMode: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+    };
+
+    if (width > 414) {
+      return (
+        <StyledBase.FlexWrapper>
+          <CardList />
+        </StyledBase.FlexWrapper>
+      );
+    }
+
+    return (
+      <Styled.SliderWrapper id="carousel">
+        <Slider {...settings}>
+          <Link to="/about">
+            <Card img={About} name="About" />
+          </Link>
+          <Card img={Join} name="Join" />
+          <Card img={BonVoyage} name="Von Voyage" />
+          <Link to="/all-ears">
+            <Card img={AllEars} name="I'm all ears" />
+          </Link>
+        </Slider>
+      </Styled.SliderWrapper>
+    );
+  }
+}
 
 Card.propTypes = {
   img: PropTypes.string.isRequired,
--- src/components/main/Styled_main.js
+++ src/components/main/Styled_main.js
@@ -1,10 +1,15 @@
 import styled from 'styled-components';
 
+const SliderWrapper = styled.div`
+  display: block;
+`;
+
 const CardWrapper = styled.div`
   margin: 1rem;
   max-width: 13rem;
 `;
 
 export default {
+  SliderWrapper,
   CardWrapper,
 };
