de2fcce3b225c7bf139eed4a49efabfe24f9def5
huskyhoochu <dfg1499@gmail.com>
2018-09-11 20:39:51 +0900

    [#19] redux-form, redux-promise 연결

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import promiseMiddleware from 'redux-promise';
 
 import ConnectedApp from './App';
 import reducers from './reducers';
 import registerServiceWorker from './registerServiceWorker';
 
-const store = createStore(reducers);
+const createStoreWithMiddleware = applyMiddleware(promiseMiddleware)(
+  createStore,
+);
 
 ReactDOM.render(
-  <Provider store={store}>
+  <Provider store={createStoreWithMiddleware(reducers)}>
     <ConnectedApp />
   </Provider>,
   document.getElementById('root'),
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
 
 // Reducers
 import controlTitle from './reducer.controlTitle';
 
 const rootReducer = combineReducers({
+  form: formReducer,
   controlTitle,
 });
 
