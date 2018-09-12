67aaa3af081a475afec6bee33ba8023413476653
huskyhoochu <dfg1499@gmail.com>
2018-09-12 16:14:03 +0900

    [#19] 인트로 문구 변경

--- src/dataConfig.js
+++ src/dataConfig.js
@@ -3,7 +3,7 @@ export default {
   fontUrl:
     'https://fonts.googleapis.com/css?family=Nanum+Myeongjo:400,700&amp;subset=korean',
   siteTitle: 'Littera',
-  introText: 'SCRIPTA MANENT VERBA VOLANT',
+  introText: '당신이 읽을 책과 읽어야 할 책',
   aboutText:
     '“그 달콤한 고독을 누가 마다할까. 우리에게 필요한 것은 고독이 아닌 고립이다. 더 철저하게 혼자가 될 것. '
     + '그것만이 우리 자신이 되는 유일한 길이라는 것을 우리는 이미 알고 있다. 엄격한 이상이 부르는 가혹한 검열을 지나, '
