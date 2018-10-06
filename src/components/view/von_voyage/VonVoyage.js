90293c4fb71ba67e415dce1a6e89de52a02f838a
huskyhoochu <dfg1499@gmail.com>
2018-10-06 14:14:31 +0900

    [#22] Von Voyage 컴포넌트에 반응형 캐러셀 설치
    - 모바일 환경에서도 main 과 마찬가지로 한 항목씩 보여주도록 함

--- src/components/view/von_voyage/VonVoyage.js
+++ src/components/view/von_voyage/VonVoyage.js
@@ -2,6 +2,7 @@ import _ from 'lodash';
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import Slider from 'react-slick';
 import { listProduct } from '../../../reducers/reducer.product';
 
 // Components
@@ -9,40 +10,95 @@ import Helmet from '../../helmet/Helmet';
 
 // Styled
 import StyledBase from '../../../styled/Base';
+import Styled from './VonVoyage.styled';
+
+const Product = ({ item }) => (
+  <Styled.ProductItem>
+    <p>1 book</p>
+    <p>X</p>
+    <p>1 month</p>
+    <p>X</p>
+    <p>{`${item.months} months`}</p>
+    <p>X</p>
+    <p>{`${item.price} KRW`}</p>
+  </Styled.ProductItem>
+);
+
+const Promotion = () => (
+  <Styled.ProductItem>
+    <p>promotion</p>
+  </Styled.ProductItem>
+);
 
 class VonVoyage extends React.Component {
+  // 창의 너비가 일정 수준 이하로 좁아지면 화면 구조를 캐러셀로 변화시킨다
+  // EventListener 가 창의 너비를 실시간으로 읽어들인다
+  constructor(props) {
+    super(props);
+    this.state = { width: window.innerWidth };
+    this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
+  }
+
   componentDidMount() {
+    this.updateWindowDimensions();
+    window.addEventListener('resize', this.updateWindowDimensions);
     const { getList } = this.props;
     getList();
   }
 
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.updateWindowDimensions);
+  }
+
+  updateWindowDimensions() {
+    this.setState({ width: window.innerWidth });
+  }
+
   renderItems() {
     const { items } = this.props;
-    return _.map(items, item => (
-      <div key={item.id}>
-        <p>1 book</p>
-        <p>X</p>
-        <p>1 month</p>
-        <p>X</p>
-        <p>{`${item.months} months`}</p>
-        <p>X</p>
-        <p>{`${item.price} KRW`}</p>
-      </div>
-    ));
+    return _.map(items, item => <Product key={item.id} item={item} />);
   }
 
   render() {
+    const { width } = this.state;
+    console.log(width);
+    const settings = {
+      dots: true,
+      arrows: false,
+      infinite: true,
+      lazyLoad: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+    };
+
+    if (width > 414) {
+      return (
+        <StyledBase.FlexWrapper>
+          <Helmet pageTitle="Von Voyage!" />
+          {this.renderItems()}
+          <Promotion />
+        </StyledBase.FlexWrapper>
+      );
+    }
+
     return (
-      <StyledBase.FlexWrapper>
+      <Styled.SliderWrapper>
         <Helmet pageTitle="Von Voyage!" />
-        {this.renderItems()}
-        <div>
-          <p>promotion</p>
-        </div>
-      </StyledBase.FlexWrapper>
+        <Slider {...settings}>
+          {this.renderItems()}
+          <Promotion />
+        </Slider>
+      </Styled.SliderWrapper>
     );
   }
 }
+
+Product.propTypes = {
+  item: PropTypes.objectOf(
+    PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+  ).isRequired,
+};
 
 VonVoyage.propTypes = {
   items: PropTypes.arrayOf(PropTypes.object).isRequired,
---
+++ src/components/view/von_voyage/VonVoyage.styled.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+const SliderWrapper = styled.div`
+  position: relative;
+  display: block;
+`;
+
+const ProductItem = styled.div`
+  text-align: center;
+  margin: 0 4rem;
+`;
+
+export default {
+  SliderWrapper,
+  ProductItem,
+};
