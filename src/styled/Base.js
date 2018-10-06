81496ab58ef93853e7d3d6bdd6c9c0a52e72fe48
huskyhoochu <dfg1499@gmail.com>
2018-10-06 17:43:46 +0900

    [#23] Remington 서체 적용

--- src/styled/Base.js
+++ src/styled/Base.js
@@ -1,7 +1,28 @@
-import styled from 'styled-components';
+import styled, { injectGlobal } from 'styled-components';
+import RemingtonBoldWoff from '../assets/fonts/SilkRemington-Bold.woff';
+import RemingtonBoldWoff2 from '../assets/fonts/SilkRemington-Bold.woff2';
+import RemingtonRegularWoff from '../assets/fonts/SilkRemington-Regular.woff';
+import RemingtonRegularWoff2 from '../assets/fonts/SilkRemington-Regular.woff2';
+
+// eslint-disable-next-line no-unused-expressions
+injectGlobal`
+  @font-face {
+    font-family: 'Silk Remington';
+    font-weight: bold;
+    src: url(${RemingtonBoldWoff}) format('woff');
+    src: url(${RemingtonBoldWoff2}) format('woff2');
+  }
+  
+  @font-face {
+    font-family: 'Silk Remington';
+    font-weight: regular;
+    src: url(${RemingtonRegularWoff}) format('woff');
+    src: url(${RemingtonRegularWoff2}) format('woff2');
+  }
+`;
 
 const App = styled.div`
-  font-family: 'Nanum Myeongjo', serif;
+  font-family: 'Silk Remington', 'Nanum Myeongjo';
   font-size: 1rem;
   line-height: 2;
 
