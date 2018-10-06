0da9d389d02a5cd443da0ab5735592abce4e34e3
huskyhoochu <dfg1499@gmail.com>
2018-10-06 14:33:20 +0900

    [#22] 캐러셀 디자인에 따른 수직중앙정렬 조정

--- src/components/view/von_voyage/VonVoyage.styled.js
+++ src/components/view/von_voyage/VonVoyage.styled.js
@@ -6,8 +6,20 @@ const SliderWrapper = styled.div`
 `;
 
 const ProductItem = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
   text-align: center;
-  margin: 0 4rem;
+  margin: auto;
+
+  @media (max-width: 414px) {
+    height: calc(100vh - 13rem);
+  }
+
+  @media (max-width: 375px) {
+    height: calc(100vh - 10rem);
+  }
 `;
 
 export default {
