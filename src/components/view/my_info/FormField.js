f953584ed64679f816a2abd6d2583a5ef9869033
huskyhoochu <dfg1499@gmail.com>
2018-10-09 14:54:57 +0900

    [#27] ResignFormField 따로 생성
    - styled-component를 직접 reduxForm의 컴포넌트로 쓰면 value 전달이 안됨
    - placeholder를 지니는 Resign 전용 FormField를 따로 제작

--- src/components/view/my_info/FormField.js
+++ src/components/view/my_info/FormField.js
@@ -3,6 +3,17 @@ import React from 'react';
 // Styled
 import Styled from './MyInfo.styled';
 
+export const ResignFormField = field => (
+  <div>
+    <Styled.ManageInput
+      type={field.type}
+      placeholder={field.placeholder}
+      {...field.input}
+      required
+    />
+  </div>
+);
+
 const FormField = field => (
   <div>
     <Styled.ManageInput type={field.type} {...field.input} required />
