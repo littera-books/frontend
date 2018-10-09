8e8c67fc5f313854aa93c571d34863fe4bb6b915
huskyhoochu <dfg1499@gmail.com>
2018-10-09 15:44:58 +0900

    [#27] ResignSurvey에 LogOut 기능 추가
    - 설문을 귀찮아서 안하고 그냥 홈으로 나가도 로그아웃된 상태를 만들기 위함

--- src/components/view/my_info/ResignSurvey.js
+++ src/components/view/my_info/ResignSurvey.js
@@ -1,12 +1,35 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { signOut } from '../../../reducers/reducer.auth';
 
 // Styled
 import StyledBase from '../../../styled/Base';
 
-const ResignSurvey = () => (
-  <StyledBase.FlexWrapper>
-    <h1>resign survey</h1>
-  </StyledBase.FlexWrapper>
-);
+class ResignSurvey extends React.Component {
+  componentDidMount() {
+    const { logOut } = this.props;
+    logOut();
+  }
 
-export default ResignSurvey;
+  render() {
+    return (
+      <StyledBase.FlexWrapper>
+        <h1>resign survey</h1>
+      </StyledBase.FlexWrapper>
+    );
+  }
+}
+
+ResignSurvey.propTypes = {
+  logOut: PropTypes.func.isRequired,
+};
+
+const mapDispatchToProps = dispatch => ({
+  logOut: () => dispatch(signOut()),
+});
+
+export default connect(
+  null,
+  mapDispatchToProps,
+)(ResignSurvey);
