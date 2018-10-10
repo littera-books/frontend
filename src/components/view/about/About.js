c5c96c66337f30891f68d42ae2d8e44c90e88389
huskyhoochu <dfg1499@gmail.com>
2018-10-10 21:40:19 +0900

    [#22] About 관련 styled 조정
    - AboutWrapper를 생성해 모바일에서 여백 확보

--- src/components/view/about/About.js
+++ src/components/view/about/About.js
@@ -5,6 +5,7 @@ import Helmet from '../../helmet/Helmet';
 
 // Styled
 import Wrapper from '../../../styled_base/Wrapper';
+import Styled from './About.styled';
 
 // Data
 import dataConfig from '../../../dataConfig';
@@ -12,7 +13,9 @@ import dataConfig from '../../../dataConfig';
 const About = () => (
   <Wrapper.FlexWrapper>
     <Helmet pageTitle="About" />
-    <p>{dataConfig.aboutText}</p>
+    <Styled.AboutWrapper>
+      <p>{dataConfig.aboutText}</p>
+    </Styled.AboutWrapper>
   </Wrapper.FlexWrapper>
 );
 
---
+++ src/components/view/about/About.styled.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+import Wrapper from '../../../styled_base/Wrapper';
+
+const AboutWrapper = styled(Wrapper.ColumnWrapper)`
+  margin: 1rem;
+`;
+
+export default {
+  AboutWrapper,
+};
