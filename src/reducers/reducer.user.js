5ea48b8b8a2b875ef7bfe1ed4c3dfa195f142292
huskyhoochu <dfg1499@gmail.com>
2018-10-11 00:07:11 +0900

    [#22] CreateUser 리듀서 생성

--- src/reducers/reducer.user.js
+++ src/reducers/reducer.user.js
@@ -5,6 +5,7 @@ import axiosInstance from './axios.instance';
 const READ_TOKEN = 'READ_TOKEN';
 const RETRIEVE_INFO = 'RETRIEVE_INFO';
 const UPDATE_INFO = 'UPDATE_INFO';
+const CREATE_USER = 'CREATE_USER';
 const DESTROY_USER = 'DESTROY_USER';
 const CLEAR_ERROR = 'CLEAR_ERROR';
 
@@ -55,6 +56,34 @@ export const updateInfo = async (payload) => {
 
   return {
     type: UPDATE_INFO,
+    response,
+    error,
+  };
+};
+
+export const createUser = async (payload) => {
+  let response;
+  let error;
+
+  try {
+    response = await axiosInstance({
+      url: '/user',
+      method: 'post',
+      data: {
+        first_name: payload.firstName,
+        last_name: payload.lastName,
+        address: payload.address,
+        phone: payload.phone,
+        email: payload.email,
+        password: payload.password2,
+      },
+    });
+  } catch (e) {
+    error = e;
+  }
+
+  return {
+    type: CREATE_USER,
     response,
     error,
   };
@@ -145,6 +174,21 @@ const reducerUpdateInfo = (state, action) => {
   });
 };
 
+const reducerCreateUser = (state, action) => {
+  if (action.error) {
+    return _.assign({}, state, {
+      ...state,
+      error: action.error,
+    });
+  }
+
+  return _.assign({}, state, {
+    ...state,
+    userId: action.response.data.id,
+    error: '',
+  });
+};
+
 const reducerDestroyUser = (state, action) => {
   if (action.error) {
     return _.assign({}, state, {
@@ -173,6 +217,8 @@ export default function reducer(state = initialState, action) {
       return reducerRetrieveInfo(state, action);
     case UPDATE_INFO:
       return reducerUpdateInfo(state, action);
+    case CREATE_USER:
+      return reducerCreateUser(state, action);
     case DESTROY_USER:
       return reducerDestroyUser(state, action);
     case CLEAR_ERROR:
