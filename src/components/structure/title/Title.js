2b242668c47ed5bd1b60cefc2b5347a35978291e
huskyhoochu <dfg1499@gmail.com>
2018-10-10 19:39:05 +0900

    [#22] Wrapper와 Img 계층 분리
    - Wrapper가 상단 전체 영역을 차지하게 함
    - Img를 따로 만들어서 얘에 대해서만 너비 컨트롤

--- src/components/structure/title/Title.js
+++ src/components/structure/title/Title.js
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
 // Styled
-import Element from '../../../styled_base/Element';
 import Styled from './Title.styled';
 
 // Images
@@ -12,7 +11,7 @@ import Logo from '../../../assets/images/title.jpg';
 const Title = ({ visibility }) => (
   <Styled.TitleWrapper style={{ visibility }}>
     <Link to="/main">
-      <Element.ResponsiveImg src={Logo} alt="Title" />
+      <Styled.TitleImg src={Logo} alt="Title" />
     </Link>
   </Styled.TitleWrapper>
 );
--- src/components/structure/title/Title.styled.js
+++ src/components/structure/title/Title.styled.js
@@ -1,25 +1,35 @@
 import styled from 'styled-components';
 import Wrapper from '../../../styled_base/Wrapper';
+import Element from '../../../styled_base/Element';
 
-const TitleWrapper = styled(Wrapper.BasicBlockWrapper)`
+const TitleWrapper = styled(Wrapper.BasicFlexWrapper)`
   position: fixed;
   top: 0;
-  left: 50%;
-  transform: translateX(-50%);
+  left: 0;
   z-index: 1;
   background-color: white;
   margin: auto;
   padding: 3rem 0;
-  width: 10rem;
+  width: 100vw;
   height: 10rem;
+  justify-content: center;
+  align-items: center;
 
   @media (max-width: 375px) {
     padding: 2rem 0;
-    width: 8rem;
     height: 8rem;
+  }
+`;
+
+const TitleImg = styled(Element.ResponsiveImg)`
+  width: 10rem;
+
+  @media (max-width: 375px) {
+    width: 8rem;
   }
 `;
 
 export default {
   TitleWrapper,
+  TitleImg,
 };
