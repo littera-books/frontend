7f6d815e683943911b70acb7d64feb21650e81ac
huskyhoochu <dfg1499@gmail.com>
2018-10-18 16:09:35 +0900

    [#24] validation 문구 수정

--- src/form/Validation.js
+++ src/form/Validation.js
@@ -1,12 +1,14 @@
 const required = value => (value ? undefined : 'Required');
-const maxLength = max => value => (value && value.length > max ? `Must be ${max} characters or less` : undefined);
+const maxLength = max => value => (value && value.length > max ? `With ${max} characters or less` : undefined);
 const maxLength20 = maxLength(20);
 const maxLength11 = maxLength(11);
-const minLength = min => value => (value && value.length < min ? `Must be ${min} characters or more` : undefined);
+const minLength = min => value => (value && value.length < min ? `With ${min} characters or more` : undefined);
 const minLength8 = minLength(8);
-const number = value => (value && Number.isNaN(Number(value)) ? 'Must be a number' : undefined);
+const number = value => (value && Number.isNaN(Number(value))
+  ? 'Please check your mobile number.'
+  : undefined);
 const email = value => (value && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value)
-  ? 'Invalid email address'
+  ? 'Please check your E-mail address.'
   : undefined);
 
 export default {
