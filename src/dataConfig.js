894e5add3a9275b633322bd5e123b55d8f2b6abe
huskyhoochu <dfg1499@gmail.com>
2018-12-17 13:07:56 +0900

    [#30] Intro 문구 변경

--- src/dataConfig.js
+++ src/dataConfig.js
@@ -9,7 +9,7 @@ export default {
   daumPostApiUrl:
     'https://ssl.daumcdn.net/dmaps/map_js_init/postcode.v2.js?autoload=false',
   siteTitle: 'Littera',
-  introText: '당신이 읽을 책과 읽어야 할 책',
+  introText: 'Scripta Manent Verba Volant',
   aboutText:
     '“그 달콤한 고독을 누가 마다할까. 우리에게 필요한 것은 고독이 아닌 고립이다. 더 철저하게 혼자가 될 것. '
     + '그것만이 우리 자신이 되는 유일한 길이라는 것을 우리는 이미 알고 있다. '
