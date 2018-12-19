72f869784f8998e81d8f646796e3e4a5a24a8c8a
huskyhoochu <dfg1499@gmail.com>
2018-12-19 12:36:38 +0900

    [#30] HomeButton 포지션 늦게 잡는 문제 해결
    - positon: sticky를 사용해 relative에 대한 절대값을 갖되 화면에
    고정되도록 함
    - relative에 대한 절대값을 갖는 덕분에 left 값을 지정해주고,
    margin-left를 calc로 주지 않고 계산된 값을 직접 줌으로써 퍼포먼스 향상

--- src/components/structure/home_button/HomeButton.styled.js
+++ src/components/structure/home_button/HomeButton.styled.js
@@ -2,11 +2,12 @@ import styled from 'styled-components';
 import Wrapper from '../../../styled_base/Wrapper';
 
 const HomeButtonWrapper = styled(Wrapper.BasicBlockWrapper)`
-  position: fixed;
+  position: sticky;
   bottom: 5rem;
+  left: 0;
   width: 3rem;
   height: 3rem;
-  margin-left: calc(44.25rem - 3rem);
+  margin-left: 41.25rem;
   opacity: 0.4;
 
   :hover {
