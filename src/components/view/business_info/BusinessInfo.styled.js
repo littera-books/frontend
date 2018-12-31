ffdd3f009c0cb67b7c943df3c251c9e6c19a42b9
huskyhoochu <dfg1499@gmail.com>
2018-12-31 18:27:10 +0900

    [#30] BusinessInfo 요소 가운데로 정렬

--- src/components/view/business_info/BusinessInfo.styled.js
+++ src/components/view/business_info/BusinessInfo.styled.js
@@ -1,14 +1,15 @@
 import styled from 'styled-components';
 import Wrapper from '../../../styled_base/Wrapper';
 
-const WidthWrapper = styled(Wrapper.BasicBlockWrapper)`
+const WidthWrapper = styled(Wrapper.ColumnWrapper)`
   width: 100%;
   font-size: 9px;
+  align-items: center;
 `;
 
 const InfoWrapper = styled(Wrapper.BasicFlexWrapper)`
   align-items: flex-start;
-  width: 100%;
+  width: 70%;
   margin-bottom: ${props => (props.noBottom ? '0' : '1.5rem')};
 `;
 
