6326d4b5911d29bfc37b73469d6ecfa34c398916
huskyhoochu <dfg1499@gmail.com>
2019-01-02 11:29:14 +0900

    [#30] my-info depth가 깊어질 경우 close button 이동 경로 변경

--- src/components/structure/close_button/CloseButton.js
+++ src/components/structure/close_button/CloseButton.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
@@ -8,18 +9,41 @@ import Styled from './CloseButton.styled';
 import Close from '../../../assets/images/cross-out.svg';
 
 class CloseButton extends React.Component {
-  shouldComponentUpdate(nextProps) {
+  state = {
+    pathname: '/',
+  };
+
+  static getDerivedStateFromProps(state) {
+    if (state.pathname !== window.location.pathname) {
+      return { pathname: window.location.pathname };
+    }
+    return null;
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
     const { visibility } = this.props;
-    return visibility !== nextProps.visibility;
+    const { pathname } = this.state;
+
+    if (visibility !== nextProps.visibility) {
+      return true;
+    }
+
+    if (pathname !== nextState.pathname) {
+      return true;
+    }
+
+    return false;
   }
 
   render() {
     const { visibility } = this.props;
+    const { pathname } = this.state;
+    const isDeepDepth = _.startsWith(pathname, '/my-info');
     return (
       <Styled.CloseButtonWrapper
         style={{ visibility: visibility ? 'visible ' : 'hidden' }}
       >
-        <Link to="/main">
+        <Link to={isDeepDepth ? '/log' : '/main'}>
           <img src={Close} alt="close-button" width="16px" height="16px" />
         </Link>
       </Styled.CloseButtonWrapper>
