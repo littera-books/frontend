60b74aeb50653ff52c084fcc2f6f7b2572605d51
huskyhoochu <dfg1499@gmail.com>
2019-01-18 23:51:41 +0900

    [#31] CloseButton 모바일 대응

--- src/components/structure/close_button/styled.js
+++ src/components/structure/close_button/styled.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import Wrapper from '../../../styled_base/Wrapper';
+import Element from '../../../styled_base/Element';
 
 const CloseButtonWrapper = styled(Wrapper.BasicBlockWrapper)`
   position: absolute;
@@ -7,6 +8,10 @@ const CloseButtonWrapper = styled(Wrapper.BasicBlockWrapper)`
   right: 0;
   z-index: 1;
   padding: 30px 0 15px 0;
+
+  @media (max-width: ${Element.MEDIA.mobileWidth}) {
+    margin-right: 1rem;
+  }
 `;
 
 export default {
